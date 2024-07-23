d9ad4ec9a910c58021a6ed62bc1ac4e45ac5675c
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-23 12:28:21 +0300

    made all error labels in a singe label

--- main_page.aspx.cs
+++ main_page.aspx.cs
@@ -32,70 +32,76 @@ namespace Requesting_system
         }
 
         protected void save_Button_Click(object sender, EventArgs e)
-{
-
-    string fileName = Path.GetFileName(FileUpload1.FileName);
-    string fileExtension = Path.GetExtension(fileName).ToLower();
-    int fileSize = FileUpload1.PostedFile.ContentLength;
-    byte[] fileBytes = FileUpload1.FileBytes;
-
-    if (String.IsNullOrEmpty(id_Box.Text))
-    {
-        id_Label.Visible = true;
-        return;
-    }
-    if (DropDownList1.SelectedValue == "Select Priority")
-    {
-        priority_Label.Visible = true;
-        return;
-    }
-    if (DropDownList1.SelectedValue == "High Priority" && String.IsNullOrEmpty(des_Box.Text))
-    {
-        des_Label.Visible = true;
-        return;
-    }
-    if (DropDownList1.SelectedValue != "High Priority")
-    {
-        des_Box.Visible = false;
-        des_Box.Text= null;
-    }
-    if (String.IsNullOrEmpty(det_Box.Text))
-    { 
-        det_Label.Visible = true;
-        return;
-    }
-    if (fileExtension != ".pdf")
-    {
-        file_label.Visible = true;
-        return;
-    }
-    if (fileSize > 21000000)
-    {
-        file_label.Text = "File must be 21 mb or less";
-        file_label.Visible = true;
-    }
-
-        string query = "INSERT INTO requests (priority, desceription, details, employee_id, [file], FileName) " +
-              "VALUES (@Priority, @Description, @Details, @Employee_id, @File, @FileName)";
-
-        using (SqlCommand cmd = new SqlCommand(/*query, con*/))
         {
 
-            cmd.Connection = con;
-            cmd.CommandText = "insert_req_info";
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@Priority", DropDownList1.SelectedValue);
-            cmd.Parameters.AddWithValue("@Description", des_Box.Text);
-            cmd.Parameters.AddWithValue("@Details", det_Box.Text);
-            cmd.Parameters.AddWithValue("@Employee_id", Convert.ToInt32(id_Box.Text));
-            cmd.Parameters.AddWithValue("@File", fileBytes);
-            cmd.Parameters.AddWithValue("@FileName", fileName);
-            cmd.ExecuteNonQuery();
-            Response.Redirect("reqlist.aspx");
-        }
-    
-   
-}   
+            string fileName = Path.GetFileName(FileUpload1.FileName);
+            string fileExtension = Path.GetExtension(fileName).ToLower();
+            int fileSize = FileUpload1.PostedFile.ContentLength;
+            byte[] fileBytes = FileUpload1.FileBytes;
+
+            if (String.IsNullOrEmpty(id_Box.Text))
+            {
+                Error_label.Text = "Please enter your ID.";
+                Error_label.Visible = true;
+                return;
+            }
+            if (DropDownList1.SelectedValue == "Select Priority")
+            {
+                Error_label.Text = "Please select a priority.";
+                Error_label.Visible = true;
+                return;
+            }
+            if (DropDownList1.SelectedValue == "High Priority" && String.IsNullOrEmpty(des_Box.Text))
+            {
+                Error_label.Text = "Please write a description.";
+                Error_label.Visible = true;
+                return;
+            }
+            if (DropDownList1.SelectedValue != "High Priority")
+            {
+                des_Box.Visible = false;
+                des_Box.Text= null;
+            }
+            if (String.IsNullOrEmpty(det_Box.Text))
+            {
+                Error_label.Text = "Plaese write some details";
+                Error_label.Visible = true;
+                return;
+            }
+            if (fileExtension != ".pdf")
+            {
+                Error_label.Text = "Plaese make sure the file is a pdf file";
+                Error_label.Visible = true;
+                return;
+            }
+            if (fileSize > 21000000)
+            {
+                Error_label.Text = "File must be 21 mb or less";
+                Error_label.Visible = true;
+            }
+
+                string query = "INSERT INTO requests (priority, desceription, details, employee_id, [file], FileName) " +
+                      "VALUES (@Priority, @Description, @Details, @Employee_id, @File, @FileName)";
+
+                using (SqlCommand cmd = new SqlCommand(/*query, con*/))
+                {
+
+                    cmd.Connection = con;
+                    cmd.CommandText = "insert_req_info";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Priority", DropDownList1.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Description", des_Box.Text);
+                    cmd.Parameters.AddWithValue("@Details", det_Box.Text);
+                    cmd.Parameters.AddWithValue("@Employee_id", Convert.ToInt32(id_Box.Text));
+                    cmd.Parameters.AddWithValue("@File", fileBytes);
+                    cmd.Parameters.AddWithValue("@FileName", fileName);
+                    cmd.ExecuteNonQuery();
+                    Response.Redirect("reqlist.aspx");
+                }
+            
+           
+           Error_label.Visible=false;
+        }   
 
        
 
