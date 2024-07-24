2b6c46d35e0f1a5d1d8230c9aa69f8f24cffeb2c
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-24 13:52:44 +0300

    Added a page related to the details button
    this page will only display one record that is related to the req_id

---
+++ Det_page.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Det_page.aspx.cs" Inherits="Requesting_system.Det_page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Details page</title>
+    <style>
+        
+        body{
+            background-color:antiquewhite;
+        }
+
+        div{
+             
+             color:mediumpurple;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Details</h1>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <asp:Button ID="Button1" runat="server" Text="Back" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Det_page.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Requesting_system
+{
+    public partial class Det_page : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=ONYX\SAUD;Initial Catalog=LEARNING;Persist Security Info=True;User ID=saud;Password=abcd");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+               // test.Text = Session["viewbaseonreq"].ToString();
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
+                disp_data();
+            }
+
+        }
+        public void disp_data()
+        {
+            String reqid = Session["viewbaseonreq"].ToString();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from requests where req_id = @req_id";
+            cmd.Parameters.AddWithValue("@req_id", reqid);
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("reqlist.aspx");
+        }
+    }
+}
